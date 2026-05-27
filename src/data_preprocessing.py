1093974476778487eeab3ed0eb7fbda0e9996f97
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:06:52 +0530

    Update data_preprocessing.py

--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,1 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
+def preprocess(df):
+
+    df = df.dropna(subset=["SHOT_RESULT"])
+
+    return df
+
+
+def encode_scale(X):
+
+    X = pd.get_dummies(X, drop_first=True)
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
+    return X
