04b63ea70315311e0890fbf8ddec814c1dda11db
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:08:02 +0530

    Update model_training.py

--- src/model_training.py
+++ src/model_training.py
@@ -1,1 +1,12 @@
+from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
+def train_models(X_train, y_train):
+
+    models = {}
+
+    models["RandomForest"] = RandomForestClassifier().fit(X_train, y_train)
+
+    models["XGBoost"] = XGBClassifier(use_label_encoder=False, eval_metric='logloss').fit(X_train, y_train)
+
+    return models
