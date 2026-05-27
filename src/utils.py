7cc6679ed1c7df78df665cd0aef8908cf4922035
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:09:01 +0530

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,13 @@
+from sklearn.model_selection import train_test_split
 
+def split_data(df):
+
+    X = df.drop("SHOT_RESULT", axis=1)
+    y = df["SHOT_RESULT"]
+
+    return X, y
+
+
+def split_train_test(X, y):
+
+    return train_test_split(X, y, test_size=0.2, random_state=42)
