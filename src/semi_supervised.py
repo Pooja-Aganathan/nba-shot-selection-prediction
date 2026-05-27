777031efb044c9b321fc2ad25055184268244906
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:07:38 +0530

    Update semi_supervised.py

--- src/semi_supervised.py
+++ src/semi_supervised.py
@@ -1,1 +1,13 @@
+from sklearn.ensemble import RandomForestClassifier
 
+def pseudo_labeling(X_labeled, y_labeled, X_unlabeled):
+
+    model = RandomForestClassifier()
+
+    # Train on labeled data
+    model.fit(X_labeled, y_labeled)
+
+    # Predict pseudo labels
+    pseudo_labels = model.predict(X_unlabeled)
+
+    return pseudo_labels
