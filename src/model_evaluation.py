e38e57c2f85fcba3d4c04d4fe0417f22dc2666ec
Pooja Aganathan <pooja110606@gmail.com>
2026-05-27 10:08:36 +0530

    Update model_evaluation.py

--- src/model_evaluation.py
+++ src/model_evaluation.py
@@ -1,1 +1,10 @@
+from sklearn.metrics import accuracy_score, f1_score
 
+def evaluate(model, X_test, y_test):
+
+    y_pred = model.predict(X_test)
+
+    return {
+        "Accuracy": accuracy_score(y_test, y_pred),
+        "F1 Score": f1_score(y_test, y_pred)
+    }
